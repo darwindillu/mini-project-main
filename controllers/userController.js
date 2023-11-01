5756ff4961f567bb41e3d5df23b25b36441923bd
Dilshad <ddillu777@gmail.com>
2023-11-01 14:22:46 +0530

    version 1.3

--- controllers/userController.js
+++ controllers/userController.js
@@ -560,14 +560,22 @@ const myOrders = async (req, res) => {
     const length = productsForFirstOrder.length
     const pages = Math.ceil(length / 5)
     const userId = req.session.userId
-
+    
     const limitedOrdersWithProducts = productsForFirstOrder.slice(a, b);
+    
+    let date1;
 
-    limitedOrdersWithProducts.forEach(order => {
-      order.date = order.date.toDateString(); 
-    });
+     limitedOrdersWithProducts.forEach(order => {
+      const isoDate = new Date(order.date); 
+      const formattedDate = isoDate.toISOString().split('T')[0]; 
+      order.date = formattedDate;
+      date1 = formattedDate
+      console.log(date1);
+    });    
+    // console.log(limitedOrdersWithProducts);
 
-    res.render('user/order', { orders: limitedOrdersWithProducts, pagenum, pages, user, userId, orderId, proId });
+
+    res.render('user/order', { orders: limitedOrdersWithProducts,date1, pagenum, pages, user, userId, orderId, proId });
   }
   catch (error) {
     console.error('Error fetching orders:', error);
